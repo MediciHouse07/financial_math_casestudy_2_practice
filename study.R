ec2c3279f94e339ccf671369244908c8f408f752
MediciHouse07 <wallacecorona163@gmail.com>
2022-04-10 15:11:19 +0800

    anotated useless codes

--- study.R
+++ study.R
@@ -15,26 +15,26 @@ library(quantmod)
 library(stringr)
 
 
-GE <- read_csv("data/GE_Group_NonAdjust.csv",
-                                col_types = cols(...1 = col_date(format = "%Y-%m-%d")))%>%
-  tk_xts(date_var = ...1)
+#GE <- read_csv("data/GE_Group_NonAdjust.csv",
+#                                col_types = cols(...1 = col_date(format = "%Y-%m-%d")))%>%
+#  tk_xts(date_var = ...1)
 
-plot(GE[,"Close"], ylab="Price",main="GE Stock", type="l")
-
-
-GE_df <- read_csv("data/GE_Group_NonAdjust.csv",
-               col_types = cols(...1 = col_date(format = "%Y-%m-%d")))
-BAC_df <- read_csv("data/BAC_Group_NonAdjust.csv",
-                  col_types = cols(...1 = col_date(format = "%Y-%m-%d")))
-
-Prices <- left_join(GE_df,BAC_df)
-
-GE_df_xts <- GE_df %>% tk_xts(date_var = ...1)
+#plot(GE[,"Close"], ylab="Price",main="GE Stock", type="l")
 
 
-BAC <- read_csv("data/BAC_Group_NonAdjust.csv",
-                col_types = cols(Open = col_skip(), High = col_skip(),
-                Low = col_skip(), Volume = col_skip()))
+#GE_df <- read_csv("data/GE_Group_NonAdjust.csv",
+#               col_types = cols(...1 = col_date(format = "%Y-%m-%d")))
+#BAC_df <- read_csv("data/BAC_Group_NonAdjust.csv",
+#                  col_types = cols(...1 = col_date(format = "%Y-%m-%d")))
+
+#Prices <- left_join(GE_df,BAC_df)
+
+#GE_df_xts <- GE_df %>% tk_xts(date_var = ...1)
+
+
+#BAC <- read_csv("data/BAC_Group_NonAdjust.csv",
+#                col_types = cols(Open = col_skip(), High = col_skip(),
+#                Low = col_skip(), Volume = col_skip()))
 
 files <- list.files(path = "data") # all the datas
 target_name <- str_split_fixed(files, "_",n=3)[,1]
