84d281d2b38c0340d7957093a8bde9305fdd4302
MediciHouse07 <wallacecorona163@gmail.com>
2022-04-10 14:34:10 +0800

    rearrange

--- study.R
+++ study.R
@@ -13,18 +13,9 @@ library(reshape2)
 library(graphics)
 library(quantmod)
 
-#load("casestudy_1_0.RData")
 
 GE <- read_csv("data/GE_Group_NonAdjust.csv",
                                 col_types = cols(...1 = col_date(format = "%Y-%m-%d")))%>%
   tk_xts(date_var = ...1)
-
-
-#names(GE)[1:1] <- "Date"
-#head(GE)
-
-#GE_Plot <- GE[c('Close')]
-#rownames(GE_Plot) <- GE$Date
-# plot(GE_Plot)
 
 plot(GE[,"Close"], ylab="Price",main="GE Stock", type="l")
