b5ce641b1d462f72d10f43ab801fe24127ab86fa
MediciHouse07 <wallacecorona163@gmail.com>
2022-04-10 15:07:02 +0800

    re-arrange data structure

--- study.R
+++ study.R
@@ -38,8 +38,12 @@ BAC <- read_csv("data/BAC_Group_NonAdjust.csv",
 
 files <- list.files(path = "data") # all the datas
 target_name <- str_split_fixed(files, "_",n=3)[,1]
-i <- 0
-while(i<=length(vec))
-{
 
+i <- 1
+while(i<=length(files))
+{
+  read_csv(paste0('data/',files[i]),
+           col_types = cols(Open = col_skip(), High = col_skip(),
+                            Low = col_skip(), Volume = col_skip()))
+  i <- i + 1
 }
