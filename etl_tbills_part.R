13567bd779cfaf646dc72c15c6f90cd40952391b
MediciHouse07 <wallacecorona163@gmail.com>
2022-04-10 16:15:29 +0800

    translate to xts back tbills data

--- etl_tbills_part.R
+++ etl_tbills_part.R
@@ -16,16 +16,22 @@ library(stringr)
 files <- list.files(path = "tbills_data") # all the datas
 target_name <- str_split_fixed(files, "_",n=3)[,1]
 
-DGS1 <- read_csv("tbills_data/DGS1_df.csv")
-
+DGS1 <- read_csv("tbills_data/DGS1_df.csv") %>% tk_xts(date_var = "...1")
 DGS5 <- read_csv("tbills_data/DGS5_df.csv")
-
 DGS10 <- read_csv("tbills_data/DGS10_df.csv")
-
-DAAA_df <- read_csv("tbills_data/DAAA_df.csv")
-
-DBAA_df <- read_csv("tbills_data/DBAA_df.csv")
-
+DGS3MO <- read_csv("tbills_data/DGS3MO_df.csv") %>% tk_xts(date_var = "...1")
+DAAA <- read_csv("tbills_data/DAAA_df.csv")
+DBAA <- read_csv("tbills_data/DBAA_df.csv")
 DCOILWTICO <- read_csv("tbills_data/DCOILWTICO_df.csv")
+
+
+fred.data0<-merge(
+  DGS3MO,
+  DGS1,
+  DGS5,
+  DGS10,
+  DAAA,
+  DBAA,
+  DCOILWTICO)["2000::2013-05"]
 
 write.csv(temp_table_dcast, file = paste0('company_stock_data/','stockprice.csv'))
